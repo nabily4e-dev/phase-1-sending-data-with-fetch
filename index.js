e4b59c04e10603217b5a13cfee2d6ccd67ddaf94
NabiL Ashbat <nbl.dev21@gmail.com>
2023-02-17 23:44:41 +0300

    Build configurationObject

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Add your code here
+const configurationObject = {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({dogName: "Byron", dogBreed: "Poodle",})
+}
