e612b360592fdd70829ab9dc62bf03b1c44424a5
NabiL Ashbat <nbl.dev21@gmail.com>
2023-02-17 23:57:58 +0300

    Start challenge

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 // Add your code here
-const formData = {
-    dogName: "Byron",
-    dogBreed: "Poodle",
-  };
-  
-  const configurationObject = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json",
-    },
-    body: JSON.stringify(formData),
-  };
-  
-  fetch("http://localhost:3000/dogs", configurationObject)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (object) {
-    console.log(object);
-  })
-  .catch(function (error) {
-    alert("Bad things! Ragnarők!");
-    console.log(error.message);
-  })
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         Accept: "application/json",
+//     },
+//     body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (object) {
+//         console.log(object);
+//     })
+//     .catch(function (error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//     });
+
